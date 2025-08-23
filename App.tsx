79a0e16effb62b9d0efd7f0d70b15ead1b131156
Mohsin Bahir <mohsinbashirr486@gmail.com>
2025-08-23 09:57:40 -0700

    made a repo

--- App.tsx
+++ App.tsx
@@ -5,20 +5,20 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { StyleSheet, Text, View, useColorScheme } from 'react-native';
+
+import { NewAppScreen } from '@react-native/new-app-screen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <Text>asdasdasdasdasdasd</Text>
     </SafeAreaProvider>
   );
 }
