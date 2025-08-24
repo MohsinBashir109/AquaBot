ddfb0e2ccb971e8cbe28d5eeacfb57300a09175e
Mohsin Bahir <mohsinbashirr486@gmail.com>
2025-08-23 23:43:10 -0700

    yes  commit

--- src/navigation/authFlow/index.tsx
+++ src/navigation/authFlow/index.tsx
@@ -1,6 +1,7 @@
 import * as Auth from '../../screens/AuthFlow';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { routes } from '../../utils/routes';
 
 const AuthStack = createNativeStackNavigator();
 export const AuthNavigation = () => {
@@ -9,9 +10,9 @@ export const AuthNavigation = () => {
       screenOptions={{ headerShown: false }}
       initialRouteName="splash"
     >
-      <AuthStack.Screen name="onBoarding" component={Auth.OnBoarding} />
-      <AuthStack.Screen name="splash" component={Auth.Splash} />
-      <AuthStack.Screen name="SignIn" component={Auth.SignIn} />
+      <AuthStack.Screen name={routes.onboarding} component={Auth.OnBoarding} />
+      <AuthStack.Screen name={routes.splash} component={Auth.Splash} />
+      <AuthStack.Screen name={routes.splash} component={Auth.SignIn} />
     </AuthStack.Navigator>
   );
 };
