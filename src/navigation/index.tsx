574ff4fcf3fe76854ac63d39a47b52ed0da791ff
Mohsin Bahir <mohsinbashirr486@gmail.com>
2025-08-25 05:11:27 -0700

    added theme

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,25 +1,29 @@
+import { NavigationContainer, ThemeContext } from '@react-navigation/native';
+
 import { AuthNavigation } from './authFlow';
-import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
+import { ThemeProvider } from '../theme/ThemeContext';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const MyStack = createNativeStackNavigator();
 
 export const MainNavigator = () => {
   return (
-    <NavigationContainer>
-      <StatusBar
-        translucent
-        backgroundColor={'transparent'}
-        barStyle={'light-content'}
-      />
+    <ThemeProvider>
+      <NavigationContainer>
+        <StatusBar
+          translucent
+          backgroundColor={'transparent'}
+          barStyle={'light-content'}
+        />
 
-      <MyStack.Navigator
-        initialRouteName="AuthFlow"
-        screenOptions={{ headerShown: false }}
-      >
-        <MyStack.Screen name="AuthFlow" component={AuthNavigation} />
-      </MyStack.Navigator>
-    </NavigationContainer>
+        <MyStack.Navigator
+          initialRouteName="AuthFlow"
+          screenOptions={{ headerShown: false }}
+        >
+          <MyStack.Screen name="AuthFlow" component={AuthNavigation} />
+        </MyStack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 };
