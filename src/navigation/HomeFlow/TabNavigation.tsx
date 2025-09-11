3c08a056922b186f3637637313ef88f3acf2016f
Mohsin Bahir <mohsinbashirr486@gmail.com>
2025-09-11 03:07:46 -0700

    added the fbottom tab navigation system

--- src/navigation/HomeFlow/TabNavigation.tsx
+++ src/navigation/HomeFlow/TabNavigation.tsx
@@ -1,19 +1,90 @@
 import * as Home from '../../screens/HomeStack';
 
+import { GetBottomTabIcons } from '../../utils/BottomtabIcons';
+import { Platform } from 'react-native';
 import React from 'react';
+import { colors } from '../../utils/colors';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { heightPixel } from '../../utils/constants';
 import { routes } from '../../utils/routes';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useThemeContext } from '../../theme/ThemeContext';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
+  const { isDark } = useThemeContext();
+
+  const insets = useSafeAreaInsets();
   return (
-    <Tab.Navigator>
-      <Tab.Screen name={routes.home} component={Home.home} />
-      <Tab.Screen name={routes.guidelines} component={Home.guidelines} />
-      <Tab.Screen name={routes.settings} component={Home.settings} />
-      <Tab.Screen name={routes.chatbot} component={Home.chatBot} />
-      <Tab.Screen name={routes.profile} component={Home.profile} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: heightPixel(60),
+        },
+        tabBarStyle: {
+          backgroundColor: colors[isDark ? 'dark' : 'light'].white,
+          flexDirection: 'row',
+          height:
+            Platform.OS === 'android'
+              ? heightPixel(60) + insets.bottom
+              : heightPixel(50) + insets.bottom,
+          alignItems: 'center',
+          paddingTop: Platform.OS === 'ios' ? heightPixel(20) : 0,
+          //  paddingBottom:  insets?.bottom ,
+        },
+      }}
+    >
+      <Tab.Screen
+        name={routes.home}
+        component={Home.home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <GetBottomTabIcons focused={focused} screenName="Home" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={routes.guidelines}
+        component={Home.guidelines}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <GetBottomTabIcons focused={focused} screenName="Guidelines" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={routes.settings}
+        component={Home.settings}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <GetBottomTabIcons focused={focused} screenName="Settings" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={routes.chatbot}
+        component={Home.chatBot}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <GetBottomTabIcons focused={focused} screenName="ChatBot" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={routes.profile}
+        component={Home.profile}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <GetBottomTabIcons focused={focused} screenName="Profile" />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
---
+++ src/utils/BottomtabIcons.tsx
@@ -0,0 +1,90 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { email, eyes, google, hide, tickCircle } from '../assets/icons/icons';
+import { fontPixel, heightPixel, widthPixel } from './constants';
+
+import { colors } from './colors';
+import { fontFamilies } from './fontfamilies';
+import { useThemeContext } from '../theme/ThemeContext';
+
+export const GetBottomTabIcons = ({
+  focused,
+  screenName,
+}: {
+  focused: boolean;
+  screenName: string;
+}) => {
+  const { isDark } = useThemeContext();
+  const getIcon = () => {
+    switch (screenName) {
+      case 'Home':
+        return email;
+      case 'ChatBot':
+        return eyes;
+      case 'Profile':
+        return google;
+      case 'Settings':
+        return hide;
+      case 'Guidelines':
+        return tickCircle;
+    }
+  };
+  return (
+    <View style={styles.simpleIcon}>
+      <Image
+        source={getIcon()}
+        style={[
+          {
+            tintColor: focused
+              ? colors[isDark ? 'dark' : 'light'].primary
+              : colors[isDark ? 'dark' : 'light'].gray2,
+          },
+          styles.iconStyle,
+        ]}
+      />
+      <Text
+        style={[
+          styles?.textstyle,
+          {
+            color: focused
+              ? colors[isDark ? 'dark' : 'light'].primary
+              : colors[isDark ? 'dark' : 'light'].gray2,
+          },
+        ]}
+      >
+        {screenName}
+        {/* {t(`BottomTabs.${screenName}`)} */}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  iconStyle: {
+    width: widthPixel(20),
+    height: heightPixel(20),
+    resizeMode: 'contain',
+    marginBottom: heightPixel(5),
+  },
+
+  gradient: {
+    borderRadius: heightPixel(25),
+    marginBottom: heightPixel(5),
+    elevation: 3,
+    shadowOffset: { width: 1, height: 3 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+
+  textstyle: {
+    width: widthPixel(75),
+    fontFamily: fontFamilies.semibold,
+    fontSize: fontPixel(12),
+    textAlign: 'center',
+  },
+
+  simpleIcon: {
+    height: heightPixel(27),
+    alignItems: 'center',
+    marginTop: heightPixel(15),
+  },
+});
